3bdb8c6e33473fa348fd86fbfba72a221b59afb8
Padma <penciljamnoreply@gmail.com>
2024-10-22 15:16:43 +0100

    adding groups and modifying the group elements

--- ViteBasics/scripts.js
+++ ViteBasics/scripts.js
@@ -3,25 +3,53 @@ import * as THREE from 'three'
 //Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-
 //SCENE
 const scene = new THREE.Scene()
+
+//Axes Helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+/**Object Group */
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+
+
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff})
+)
+cube3.position.x = 2
+group.add(cube3)
+
+//Object Size
 const sizes = {
     width: 800,
     height: 600
 }
-
-//OBJECT (Mesh and Camera)
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+//Camera to view
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z =  3
 scene.add(camera)
 
-
-
+//Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
