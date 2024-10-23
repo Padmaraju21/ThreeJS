4ab91bd7224d3451fcd81113949ece3dff53bcdb
Padma <penciljamnoreply@gmail.com>
2024-10-23 11:02:56 +0100

    disabling groups changes

--- ViteBasics/scripts.js
+++ ViteBasics/scripts.js
@@ -7,35 +7,41 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 //Axes Helper
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
+
+/** Single Object*/
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 /**Object Group */
-const group = new THREE.Group()
-group.position.y = 1
-group.scale.y = 2
-group.rotation.y = 1
-scene.add(group)
+// const group = new THREE.Group()
+// group.position.y = 1
+// group.scale.y = 2
+// group.rotation.y = 1
+// scene.add(group)
 
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: 0xff0000})
-)
-group.add(cube1)
+// const cube1 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({color: 0xff0000})
+// )
+// group.add(cube1)
 
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: 0x00ff00})
-)
-cube2.position.x = -2
-group.add(cube2)
+// const cube2 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({color: 0x00ff00})
+// )
+// cube2.position.x = -2
+// group.add(cube2)
 
-const cube3 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({color: 0x0000ff})
-)
-cube3.position.x = 2
-group.add(cube3)
+// const cube3 = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({color: 0x0000ff})
+// )
+// cube3.position.x = 2
+// group.add(cube3)
 
 //Object Size
 const sizes = {
